eb7cabc02540638a3c81925820dd1b8f2e335ec9
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-18 23:18:22 +0000

    Update join.tsx

--- prodev-mobile-app-0x04/app/join.tsx
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -1,1 +1,28 @@
+import { View, Text, TextInput, Pressable } from "react-native";
+import { styles } from "@/styles/_joinstyle";
 
+export default function Join() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleTextGroup}>
+        <Text style={styles.titleText}>Join</Text>
+        <Text style={styles.subText}>Create a new account</Text>
+      </View>
+
+      <View style={styles.formGroup}>
+        <Text style={styles.formLabel}>Full Name</Text>
+        <TextInput style={styles.formControl} />
+
+        <Text style={styles.formLabel}>Email</Text>
+        <TextInput style={styles.formControl} />
+
+        <Text style={styles.formLabel}>Password</Text>
+        <TextInput style={styles.formControl} secureTextEntry />
+      </View>
+
+      <Pressable style={styles.primaryButton}>
+        <Text style={styles.buttonText}>Create Account</Text>
+      </Pressable>
+    </View>
+  );
+}

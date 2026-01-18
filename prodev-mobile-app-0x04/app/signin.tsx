807c5c9e7a8d3af042e1c7f369fc7f6e0a78d7b3
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-18 23:18:08 +0000

    Update signin.tsx

--- prodev-mobile-app-0x04/app/signin.tsx
+++ prodev-mobile-app-0x04/app/signin.tsx
@@ -1,1 +1,25 @@
+import { View, Text, TextInput, Pressable } from "react-native";
+import { styles } from "@/styles/_joinstyle";
 
+export default function SignIn() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleTextGroup}>
+        <Text style={styles.titleText}>Sign In</Text>
+        <Text style={styles.subText}>Welcome back</Text>
+      </View>
+
+      <View style={styles.formGroup}>
+        <Text style={styles.formLabel}>Email</Text>
+        <TextInput style={styles.formControl} />
+
+        <Text style={styles.formLabel}>Password</Text>
+        <TextInput style={styles.formControl} secureTextEntry />
+      </View>
+
+      <Pressable style={styles.primaryButton}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </Pressable>
+    </View>
+  );
+}
